02e9079d8c66b7e2feabfeaffd797f5da7ff7704
Fred He <fhe@bayviewglen.ca>
2023-12-29 23:27:05 -0800

    init

---
+++ typing-test/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import HelloWorld from './components/HelloWorld'
+
+function App() {
+  return <div><HelloWorld /></div>
+}
+
+export default App
